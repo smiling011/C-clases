a02add93be2e59ead7141751f102f57441e28112
Victoria Vielma <victoriasaleck@gmail.com>
2026-01-19 17:54:29 -0500

    Repaso de la primera clase

---
+++ app/ejercicio2.cs
@@ -0,0 +1,46 @@
+
+using System;
+
+namespace CSharpHelloWorld
+{
+        class HelloWorld
+    {
+        static void Main(string[] agres)
+        {
+            // Console.WriteLine("Hola, que tal!!");
+            // Console.WriteLine("Hola, que tal!!");
+            // Console.WriteLine("Hola, que tal!!");
+            // Console.WriteLine("Hola, que tal!!");
+            // Console.WriteLine("Hola, que tal!!");
+
+            string myString = "Cadena de texto";
+            myString = "Combio la cadena";
+            Console.WriteLine(myString);
+
+            int myInt = 7;
+            myInt =myIntInt + 4;
+            Console.WriteLine(myInt);
+            Console.WriteLine(myInt - 1);
+            Console.WriteLine(myInt);
+
+            double myDouble = 6.5;
+            Console.WriteLine(myDouble);
+
+            float myFloat = 6.5f;
+            Console.WriteLine(myFloat);
+
+            dynamic myDynamic = 6;
+            myDynamic = "Mi dato dinamico";
+            Console.WriteLine(myDinamic + myFloat);
+
+            var myVar = "mi variable de tipo inferido";
+            // myVar = 6; error error
+            Console.WriteLine(myVar);
+
+            console.WriteLine($"Elvalor de mi entero es {myInt} y el de mi bool {myBool}");
+
+        }
+    }
+}
+
+
