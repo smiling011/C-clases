f1419771c985f173d9befca2bde90870fce16831
Victoria Vielma <victoriasaleck@gmail.com>
2026-01-19 15:12:41 -0500

    Actividad en c#

---
+++ Trabajo en Visual Studio .net y c#/Circulo/Circulo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables declaradas
+            var radio = 0d;
+            var resultado = 0d;
+
+            //constante 
+            //const double Pi = 3.14;
+
+            //print del mensaje al user
+            Console.WriteLine("Bienvenido a Calculadora circulo");
+
+            //Mensaje al userque lee lo que ingrasa 
+            Console.WriteLine("ingrese el valor del radio");
+            radio = Convert.ToDouble(Console.ReadLine());
+
+            //proceso que hace para calcular el problema
+            resultado = Math.PI * (radio * radio);
+
+            //imprime el resultado
+            Console.WriteLine("El area de tu circulo es de: "+resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Trabajo en Visual Studio .net y c#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hola mundo en visual studio
+            Console.WriteLine("Hello, World!");
+
+            //programa para calcular el area de un rectangulo
+            int ladoA;
+            int ladoB;
+            int resultado;
+
+            Console.WriteLine("Calcula  el area de un rectangulo");
+            Console.WriteLine("Ingrese el valero del lado A");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor del lado B");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            resultado = ladoA * ladoB;
+            Console.WriteLine(resultado);
+
+
+
+
+
+
+            // para mntener consola de copilacion abierta
+            Console.ReadKey();
+
+
+        }
+    }
+}
