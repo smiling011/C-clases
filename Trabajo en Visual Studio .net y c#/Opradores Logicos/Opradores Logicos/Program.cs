cfe05904cf40acdac53a91c3a2a04e633bfd56de
Victoria Vielma <victoriasaleck@gmail.com>
2026-01-20 09:31:45 -0500

    Actualizacion de problema en console

---
+++ Trabajo en Visual Studio .net y c#/Opradores Logicos/Opradores Logicos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opradores_Logicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //funcionalidades logicas
+
+            //&& and
+            //|| or
+            //!
+            //^
+
+            //declaracion normal
+
+            //bool valor1 = true;
+            //bool valor2 = false;
+            //bool valor3 = true;
+
+            //declaracion doble
+            var (valor1, valor2, valor3) = (true, true, false);
+
+
+            bool resultAnd = valor1 && valor2 && valor3;
+            Console.WriteLine(resultAnd);
+
+            bool resultOr = valor1 || valor2 || valor3;
+            Console.WriteLine(resultOr);
+
+            bool resultAndOr = (valor1 && valor2) || valor3;
+            Console.WriteLine(resultAndOr);
+
+            bool resultNot = !valor1;
+            Console.WriteLine(resultNot);
+
+            bool resultXor = (valor1 ^ valor2) ^ valor3;
+            Console.WriteLine(resultXor);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
